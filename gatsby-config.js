a98d8983b7e9eed2a211e1ace84e0494c7a09cfd
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2021-02-15 23:10:58 +0100

    Add dotenv config to gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,33 +1,36 @@
+let activeEnv =
+	process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development";
+
+console.log(`Using environment config: ${activeEnv}`);
+
+require("dotenv").config({
+	path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
-  siteMetadata: {
-    title: "GIG Media",
-  },
-  plugins: [
-    "gatsby-plugin-sass",
-    {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "",
-      },
-    },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+	siteMetadata: {
+		title: "GIG Media",
+	},
+	plugins: [
+		"gatsby-plugin-sass",
+		"gatsby-plugin-sharp",
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sitemap",
+		"gatsby-plugin-offline",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				icon: "src/assets/images/icon.png",
+			},
+		},
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/assets/images/",
+			},
+			__key: "images",
+		},
+	],
 };
