a8eb776e91cd9cb6957e0b4fc74adf11d03bc051
Anu Alabi <anuoluwapo.alabi20@gmail.com>
2021-02-17 05:41:09 +0100

    Added token env var to API request

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -3,6 +3,6 @@ import axios from "axios";
 export const fetchCompanyApi = axios.create({
 	baseURL: process.env.COMPANY_API,
 	headers: {
-		token: `aaEe20K65nsGHEsQyrWWAtWE6SX81R8Lqv5tOgax4zNuaaNvL8nsA6X1N337PVBZKodyrc`,
+		token: `${process.env.API_TOKEN}`,
 	},
 });
